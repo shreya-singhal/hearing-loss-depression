1b4f9b2912de095f42390a27d8ac26ea0c4fe5e1
Shreya Singhal <43446379+shreya-singhal@users.noreply.github.com>
2019-08-15 15:32:59 -0400

    Update hearing_depression_analysis_P1.R

--- analysis/hearing_depression_analysis_P1.R
+++ analysis/hearing_depression_analysis_P1.R
@@ -1,4 +1,4 @@
-#Required Packages
+# Required Packages:
 library(imputeTS)
 library(ggpubr)
 library(dplyr)
@@ -8,12 +8,14 @@ library(knitr)
 library(apaTables)
 library(car)
 
-load("C:/Users/ss12293/Box Sync/Hearing Depression Project/hearing_data_subset.Rdata")
+load("C:/Users/ss12293/Box Sync/Hearing Depression Project/hearing_data_subset.Rdata") #change to working directory
 
-#Exclude anyone who wears a hearing aid.
+# The 2004 wave of HRS data will be used, as the sample size is largest. 
+
+# Exclude anyone who wears a hearing aid in 2004.
 Q1_data <- hearing_data_subset %>% filter(hearing_aid_04 == 5 & hearing_aid_PL_06 == 5 & hearing_04 != 8 & hearing_04 != 9)
 
-#Mean-Item Imputation (any NAs are replaced with the mean)
+# Mean Item Imputation (any NAs are replaced with the mean of that column)
 Q1_data$felt_depressed_04 <- na_mean(Q1_data$felt_depressed_04)
 Q1_data$effort_04 <- na_mean(Q1_data$effort_04)
 Q1_data$sleep_restless_04 <- na_mean(Q1_data$sleep_restless_04)
@@ -24,31 +26,29 @@ Q1_data$felt_sad_04 <- na_mean(Q1_data$felt_sad_04)
 Q1_data$not_going_04 <- na_mean(Q1_data$not_going_04)
 
 
-#CES-D score is the sum of these 8 items. 
+# CES-D score is the sum of the 8 items from above. 
 Q1_data <- Q1_data %>% rowwise() %>% mutate(CESD_04 = sum(felt_depressed_04,effort_04, sleep_restless_04,
                                                                             happy_04, lonely_04, enjoyed_life_04,
                                                                             felt_sad_04, not_going_04))
 
-#Self-reported hearing rating is a factor variable.
+# Self-reported hearing rating is changed to a categorical variable. 
 Q1_data$hearing_04 <- as.factor(Q1_data$hearing_04)
 levels(Q1_data$hearing_04) <- c("1" = "Excellent", "2" = "Very Good", "3" = "Good", "4" = "Fair", "5" = "Poor")
-levels(Q1_data$hearing_04)
 
 
-#Race
+#Race (a covariate) is changed to a categorical variable. 
 Q1_data$race <- as.factor(Q1_data$race)
 
-#Cognitive Impairment (Yes/No)
+#Cognitive Impairment (another covariate) (Yes/No)
 Q1_data$cogImp_04 <- NA
 Q1_data$cogImp_04[Q1_data$cog_function_04 == 1] <- 0 #No 
 Q1_data$cogImp_04[Q1_data$cog_function_04 == 2 | Q1_data$cog_function_04 == 3] <- 1 #Yes 
 
 
 
-#######################Summary Statistics
+####################### Summary Statistics
 
-
-#Mean CESD per hearing level
+# CES-D Summary Statistics per hearing level
 hearing_level_CESD_table <- group_by(Q1_data, hearing_04) %>%
   summarise(
     count = n(),
@@ -59,24 +59,24 @@ hearing_level_CESD_table <- group_by(Q1_data, hearing_04) %>%
 hearing_level_CESD_table %>% kable(col.names = c("", "count", "mean", "sd"), caption = "Rate Hearing 2004") %>% 
   kable_styling(bootstrap_options = c("striped", "hover"), full_width = F)
 
-
-leveneTest(CESD_04 ~ hearing_04, data = Q1_data) #variances not equal, therefore cannot use ANOVA
-
-#Use Kruskal Test instead of ANOVA
-kruskal.test(CESD_04 ~ hearing_04, data = Q1_data)
-
-#Pairwise Wilcoxon to determine which pairs are significant 
-pairwise.wilcox.test(Q1_data$CESD_04, Q1_data$hearing_04, p.adjust.method = "BH")
-
-
+# Boxplot to show distribution of CES-D scores for each hearing level
 ggboxplot(Q1_data, x = "hearing_04", y = "CESD_04", 
           color = "hearing_04")
 
+# Use a Levene test to check for equality of variances:
 
-summary(Q1_data$CESD_04)
+leveneTest(CESD_04 ~ hearing_04, data = Q1_data) #variances not equal, therefore cannot use ANOVA
 
+# Use Kruskal Test instead of ANOVA:
+kruskal.test(CESD_04 ~ hearing_04, data = Q1_data)
 
-#Mean CESD per hearing prob or not 
+# Use Pairwise Wilcoxon Rank Sum test to determine which pairs are significant:
+pairwise.wilcox.test(Q1_data$CESD_04, Q1_data$hearing_04, p.adjust.method = "BH")
+
+########################################################
+
+# Hearing impairment means a score of 4 or 5.  
+
 hearing_prob_CESD_table <- group_by(Q1_data, hearing_prob_04) %>%
   summarise(
     count = n(),
@@ -84,18 +84,20 @@ hearing_prob_CESD_table <- group_by(Q1_data, hearing_prob_04) %>%
     sd = sd(CESD_04)
   )
 
-
-
+#CES-D Scores per hearing impairment or not
 hearing_prob_CESD_table %>% kable(col.names = c("", "count", "mean", "sd"),
                                   caption = "Hearing Impairment (2004)") %>% 
   kable_styling(bootstrap_options = c("striped", "hover"), full_width = F)
 
 
+# Boxplot to show distribution of CES-D scores for hearing impairment or not 
 
 ggboxplot(Q1_data, x = "hearing_prob_04", y = "CESD_04", 
           color = "hearing_prob_04", palette = c("#00AFBB", "#E7B800"),
           ylab = "CESD Score", xlab = "Hearing Impairment")
 
+
+# T-test to test for the hypothesis that the mean CES-D score among those who are not hearing impaired is lower than the mean CES-D score among those who are hearing impaired.
 
 t.test(CESD_04 ~ hearing_prob_04, data = Q1_data, var.equal = FALSE, alternative = "less")
 
